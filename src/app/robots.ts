566e4f7f85323993a995fb47cce9f0e854c94973
joniavni-hash <joni.avni@gmail.com>
2026-02-23 09:40:33 +0200

    Fix robots.txt URL: villalithos.com -> www.villalithosgreece.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,9 @@
 import { MetadataRoute } from "next";
 
+
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://villalithos.com";
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.villalithosgreece.com";
+
 
   return {
     rules: {
@@ -12,3 +14,4 @@ export default function robots(): MetadataRoute.Robots {
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
+
