a84b6f1138ba5dbfa81109304f2db40965cc4268
joniavni-hash <joni.avni@gmail.com>
2026-02-24 18:40:39 +0200

    Fix Articles nav href to include /articles path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const DEFAULT_NAV_LINKS = [
   { href: "/#services", label: "Concierge" },
   { href: "/#gallery", label: "Gallery" },
   { href: "/#location", label: "Location" },
-  { href: "https://blog.villalithosgreece.com", label: "Articles" },
+  { href: "https://blog.villalithosgreece.com/articles", label: "Articles" },
 ];
 
 export default function Header({ data }: { data?: HeaderData }) {
