48ce6d1c368ef9d22adc06bd7cf4d7e12f905644
joniavni-hash <joni.avni@gmail.com>
2026-03-03 23:05:33 +0200

    Add 301 redirect for porto-rafti-family-holiday to correct URL

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+    async redirects() {
+          return [
+            {
+                      source: '/articles/porto-rafti-family-holiday',
+                      destination: '/articles/porto-rafti-family-holiday-greece',
+                      permanent: true,
+            },
+                ];
+    },
 };
 
 export default nextConfig;
